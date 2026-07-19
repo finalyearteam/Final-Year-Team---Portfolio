4280dd4f3ee4d41fa51fe8574c65f8d1f5e6f90f
Sumit Lasulkar <sumit.lasulkar4u@gmail.com>
2026-07-20 00:19:50 +0530

    Initial commit

--- components/header.tsx
+++ components/header.tsx
@@ -59,7 +59,7 @@ export default function Header() {
         {/* Mobile Dropdown (Visible only when button is clicked on Mobile) */}
         {isMobileOpen && (
           <div className={styles.mobileMenu}>
-            <Link href="/home" className={styles.navLink} onClick={closeMenu}>Home</Link>
+            <Link href="/" className={styles.navLink} onClick={closeMenu}>Home</Link>
             <Link href="#about" className={styles.navLink} onClick={closeMenu}>About</Link>
             <Link href="#team" className={styles.navLink} onClick={closeMenu}>Team</Link>
             <Link href="#contact" className={styles.navLink} onClick={closeMenu}>Contact</Link>
